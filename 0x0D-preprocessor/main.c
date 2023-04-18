0bf04a1c45cc9e0ad0dba55a79a7541efcb8ab81
Giorhoma <giorhoma19@gmail.com>
2023-04-18 22:32:48 +0100

    Preprocessor

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* main - Write a program that prints the name of the file it was compiled from, followed by a new line.
+* Return: 0
+*/
+
+int main(void)
+{
+        printf(2-main.c)
+
+        return (0);
+}
